3375b2dbb1bbc6df08b4b15bf9f06dd1a94e2143
Mamoru <senkinsaha@mail.ru>
2023-06-01 21:02:08 +0400

    FirstDay

--- app/src/main/java/com/example/computershopmobile/Models/User.java
+++ app/src/main/java/com/example/computershopmobile/Models/User.java
@@ -2,5 +2,28 @@ package com.example.computershopmobile.Models;
 
 public class User
 {
+    public int Id;
+    public String Login;
+    public String Password;
+    public String Name;
+    public String LastName;
+    public String SurName;
+    public String Email;
+    public byte[] Avatar;
 
+    public User(){}
+
+    public void setLogin(String Login){this.Login = Login;}
+
+    public void setPassword(String Password){this.Password = Password;}
+
+    public void setName(String Name){this.Name = Name;}
+
+    public void setLastName(String LastName){this.LastName = LastName;}
+
+    public void setSurName(String SurName){this.SurName = SurName;}
+
+    public void setEmail(String Email){this.Email = Email;}
+
+    public void setAvatar(byte[] Avatar){this.Avatar = Avatar;}
 }
--- app/src/main/java/com/example/computershopmobile/RegistrationActivity.java
+++ app/src/main/java/com/example/computershopmobile/RegistrationActivity.java
@@ -4,11 +4,22 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
+
 public class RegistrationActivity extends AppCompatActivity {
 
+    ManagedChannel channel = ManagedChannelBuilder.forTarget("5.3.65.108:8080").usePlaintext().build();
+
+    //Client.CreateUserRequest
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
+        loadData();
+    }
+
+    public void loadData() {
+
     }
 }
