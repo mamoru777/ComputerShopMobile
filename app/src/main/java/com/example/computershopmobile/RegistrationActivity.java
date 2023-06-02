7becbc881cf443ba871a0b3c1bef5d9b434001e4
Mamoru777 <senkinsaha@mail.ru>
2023-06-02 17:23:04 +0400

    im tired

--- app/src/main/java/com/example/computershopmobile/RegistrationActivity.java
+++ app/src/main/java/com/example/computershopmobile/RegistrationActivity.java
@@ -2,16 +2,29 @@ package com.example.computershopmobile;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 import io.grpc.ManagedChannel;
 import io.grpc.ManagedChannelBuilder;
 
+import com.example.computershopmobile.proto.Client;
+import com.example.computershopmobile.proto.UserServiceGrpc;
+
+
 public class RegistrationActivity extends AppCompatActivity {
 
-    ManagedChannel channel = ManagedChannelBuilder.forTarget("5.3.65.108:8080").usePlaintext().build();
 
+
+    //Client.CreateUserRequest request = Client.CreateUserRequest.newBuilder().
     //Client.CreateUserRequest
+
+    Button buttonRegistr;
+    EditText EditTestLogin;
+    EditText EditTextPassword;
+    EditText EditTextEmail;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,6 +33,19 @@ public class RegistrationActivity extends AppCompatActivity {
     }
 
     public void loadData() {
+        ManagedChannel channel = ManagedChannelBuilder.forTarget("5.3.65.108:8080").usePlaintext().build();
+        UserServiceGrpc.UserServiceBlockingStub stub = UserServiceGrpc.newBlockingStub(channel);
+        EditTestLogin = findViewById(R.id.editTextLoginReg);
+        EditTextPassword = findViewById(R.id.editTextPasswordReg);
+        EditTextEmail = findViewById(R.id.editTextEmailReg);
+        buttonRegistr = findViewById(R.id.buttonRegistrReg);
 
+        buttonRegistr.setOnClickListener(v -> {
+            Client.CreateUserRequest request = Client.CreateUserRequest.newBuilder().setLogin(EditTestLogin.getText().toString()).setPassword(EditTextPassword.getText().toString()).setEmail(EditTextEmail.getText().toString()).build();
+            stub.createUser(request);
+            Intent intent = new Intent(RegistrationActivity.this, LoginActivity.class);
+                }
+
+        );
     }
 }
---
+++ app/src/main/java/com/example/computershopmobile/proto/UserServiceGrpc.java
@@ -0,0 +1,256 @@
+package com.example.computershopmobile.proto;
+
+import static io.grpc.MethodDescriptor.generateFullMethodName;
+
+/**
+ */
+@javax.annotation.Generated(
+    value = "by gRPC proto compiler (version 1.54.1)",
+    comments = "Source: Client.proto")
+@io.grpc.stub.annotations.GrpcGenerated
+public final class UserServiceGrpc {
+
+  private UserServiceGrpc() {}
+
+  public static final String SERVICE_NAME = "UserService";
+
+  // Static method descriptors that strictly reflect the proto.
+  private static volatile io.grpc.MethodDescriptor<Client.CreateUserRequest,
+      Client.CreateUserResponse> getCreateUserMethod;
+
+  @io.grpc.stub.annotations.RpcMethod(
+      fullMethodName = SERVICE_NAME + '/' + "CreateUser",
+      requestType = Client.CreateUserRequest.class,
+      responseType = Client.CreateUserResponse.class,
+      methodType = io.grpc.MethodDescriptor.MethodType.UNARY)
+  public static io.grpc.MethodDescriptor<Client.CreateUserRequest,
+      Client.CreateUserResponse> getCreateUserMethod() {
+    io.grpc.MethodDescriptor<Client.CreateUserRequest, Client.CreateUserResponse> getCreateUserMethod;
+    if ((getCreateUserMethod = UserServiceGrpc.getCreateUserMethod) == null) {
+      synchronized (UserServiceGrpc.class) {
+        if ((getCreateUserMethod = UserServiceGrpc.getCreateUserMethod) == null) {
+          UserServiceGrpc.getCreateUserMethod = getCreateUserMethod =
+              io.grpc.MethodDescriptor.<Client.CreateUserRequest, Client.CreateUserResponse>newBuilder()
+              .setType(io.grpc.MethodDescriptor.MethodType.UNARY)
+              .setFullMethodName(generateFullMethodName(SERVICE_NAME, "CreateUser"))
+              .setSampledToLocalTracing(true)
+              .setRequestMarshaller(io.grpc.protobuf.lite.ProtoLiteUtils.marshaller(
+                  Client.CreateUserRequest.getDefaultInstance()))
+              .setResponseMarshaller(io.grpc.protobuf.lite.ProtoLiteUtils.marshaller(
+                  Client.CreateUserResponse.getDefaultInstance()))
+              .build();
+        }
+      }
+    }
+    return getCreateUserMethod;
+  }
+
+  /**
+   * Creates a new async stub that supports all call types for the service
+   */
+  public static UserServiceStub newStub(io.grpc.Channel channel) {
+    io.grpc.stub.AbstractStub.StubFactory<UserServiceStub> factory =
+      new io.grpc.stub.AbstractStub.StubFactory<UserServiceStub>() {
+        @Override
+        public UserServiceStub newStub(io.grpc.Channel channel, io.grpc.CallOptions callOptions) {
+          return new UserServiceStub(channel, callOptions);
+        }
+      };
+    return UserServiceStub.newStub(factory, channel);
+  }
+
+  /**
+   * Creates a new blocking-style stub that supports unary and streaming output calls on the service
+   */
+  public static UserServiceBlockingStub newBlockingStub(
+      io.grpc.Channel channel) {
+    io.grpc.stub.AbstractStub.StubFactory<UserServiceBlockingStub> factory =
+      new io.grpc.stub.AbstractStub.StubFactory<UserServiceBlockingStub>() {
+        @Override
+        public UserServiceBlockingStub newStub(io.grpc.Channel channel, io.grpc.CallOptions callOptions) {
+          return new UserServiceBlockingStub(channel, callOptions);
+        }
+      };
+    return UserServiceBlockingStub.newStub(factory, channel);
+  }
+
+  /**
+   * Creates a new ListenableFuture-style stub that supports unary calls on the service
+   */
+  public static UserServiceFutureStub newFutureStub(
+      io.grpc.Channel channel) {
+    io.grpc.stub.AbstractStub.StubFactory<UserServiceFutureStub> factory =
+      new io.grpc.stub.AbstractStub.StubFactory<UserServiceFutureStub>() {
+        @Override
+        public UserServiceFutureStub newStub(io.grpc.Channel channel, io.grpc.CallOptions callOptions) {
+          return new UserServiceFutureStub(channel, callOptions);
+        }
+      };
+    return UserServiceFutureStub.newStub(factory, channel);
+  }
+
+  /**
+   */
+  public interface AsyncService {
+
+    /**
+     */
+    default void createUser(Client.CreateUserRequest request,
+                            io.grpc.stub.StreamObserver<Client.CreateUserResponse> responseObserver) {
+      io.grpc.stub.ServerCalls.asyncUnimplementedUnaryCall(getCreateUserMethod(), responseObserver);
+    }
+  }
+
+  /**
+   * Base class for the server implementation of the service UserService.
+   */
+  public static abstract class UserServiceImplBase
+      implements io.grpc.BindableService, AsyncService {
+
+    @Override public final io.grpc.ServerServiceDefinition bindService() {
+      return UserServiceGrpc.bindService(this);
+    }
+  }
+
+  /**
+   * A stub to allow clients to do asynchronous rpc calls to service UserService.
+   */
+  public static final class UserServiceStub
+      extends io.grpc.stub.AbstractAsyncStub<UserServiceStub> {
+    private UserServiceStub(
+        io.grpc.Channel channel, io.grpc.CallOptions callOptions) {
+      super(channel, callOptions);
+    }
+
+    @Override
+    protected UserServiceStub build(
+        io.grpc.Channel channel, io.grpc.CallOptions callOptions) {
+      return new UserServiceStub(channel, callOptions);
+    }
+
+    /**
+     */
+    public void createUser(Client.CreateUserRequest request,
+                           io.grpc.stub.StreamObserver<Client.CreateUserResponse> responseObserver) {
+      io.grpc.stub.ClientCalls.asyncUnaryCall(
+          getChannel().newCall(getCreateUserMethod(), getCallOptions()), request, responseObserver);
+    }
+  }
+
+  /**
+   * A stub to allow clients to do synchronous rpc calls to service UserService.
+   */
+  public static final class UserServiceBlockingStub
+      extends io.grpc.stub.AbstractBlockingStub<UserServiceBlockingStub> {
+    private UserServiceBlockingStub(
+        io.grpc.Channel channel, io.grpc.CallOptions callOptions) {
+      super(channel, callOptions);
+    }
+
+    @Override
+    protected UserServiceBlockingStub build(
+        io.grpc.Channel channel, io.grpc.CallOptions callOptions) {
+      return new UserServiceBlockingStub(channel, callOptions);
+    }
+
+    /**
+     */
+    public Client.CreateUserResponse createUser(Client.CreateUserRequest request) {
+      return io.grpc.stub.ClientCalls.blockingUnaryCall(
+          getChannel(), getCreateUserMethod(), getCallOptions(), request);
+    }
+  }
+
+  /**
+   * A stub to allow clients to do ListenableFuture-style rpc calls to service UserService.
+   */
+  public static final class UserServiceFutureStub
+      extends io.grpc.stub.AbstractFutureStub<UserServiceFutureStub> {
+    private UserServiceFutureStub(
+        io.grpc.Channel channel, io.grpc.CallOptions callOptions) {
+      super(channel, callOptions);
+    }
+
+    @Override
+    protected UserServiceFutureStub build(
+        io.grpc.Channel channel, io.grpc.CallOptions callOptions) {
+      return new UserServiceFutureStub(channel, callOptions);
+    }
+
+    /**
+     */
+    public com.google.common.util.concurrent.ListenableFuture<Client.CreateUserResponse> createUser(
+        Client.CreateUserRequest request) {
+      return io.grpc.stub.ClientCalls.futureUnaryCall(
+          getChannel().newCall(getCreateUserMethod(), getCallOptions()), request);
+    }
+  }
+
+  private static final int METHODID_CREATE_USER = 0;
+
+  private static final class MethodHandlers<Req, Resp> implements
+      io.grpc.stub.ServerCalls.UnaryMethod<Req, Resp>,
+      io.grpc.stub.ServerCalls.ServerStreamingMethod<Req, Resp>,
+      io.grpc.stub.ServerCalls.ClientStreamingMethod<Req, Resp>,
+      io.grpc.stub.ServerCalls.BidiStreamingMethod<Req, Resp> {
+    private final AsyncService serviceImpl;
+    private final int methodId;
+
+    MethodHandlers(AsyncService serviceImpl, int methodId) {
+      this.serviceImpl = serviceImpl;
+      this.methodId = methodId;
+    }
+
+    @Override
+    @SuppressWarnings("unchecked")
+    public void invoke(Req request, io.grpc.stub.StreamObserver<Resp> responseObserver) {
+      switch (methodId) {
+        case METHODID_CREATE_USER:
+          serviceImpl.createUser((Client.CreateUserRequest) request,
+              (io.grpc.stub.StreamObserver<Client.CreateUserResponse>) responseObserver);
+          break;
+        default:
+          throw new AssertionError();
+      }
+    }
+
+    @Override
+    @SuppressWarnings("unchecked")
+    public io.grpc.stub.StreamObserver<Req> invoke(
+        io.grpc.stub.StreamObserver<Resp> responseObserver) {
+      switch (methodId) {
+        default:
+          throw new AssertionError();
+      }
+    }
+  }
+
+  public static final io.grpc.ServerServiceDefinition bindService(AsyncService service) {
+    return io.grpc.ServerServiceDefinition.builder(getServiceDescriptor())
+        .addMethod(
+          getCreateUserMethod(),
+          io.grpc.stub.ServerCalls.asyncUnaryCall(
+            new MethodHandlers<
+              Client.CreateUserRequest,
+              Client.CreateUserResponse>(
+                service, METHODID_CREATE_USER)))
+        .build();
+  }
+
+  private static volatile io.grpc.ServiceDescriptor serviceDescriptor;
+
+  public static io.grpc.ServiceDescriptor getServiceDescriptor() {
+    io.grpc.ServiceDescriptor result = serviceDescriptor;
+    if (result == null) {
+      synchronized (UserServiceGrpc.class) {
+        result = serviceDescriptor;
+        if (result == null) {
+          serviceDescriptor = result = io.grpc.ServiceDescriptor.newBuilder(SERVICE_NAME)
+              .addMethod(getCreateUserMethod())
+              .build();
+        }
+      }
+    }
+    return result;
+  }
+}
